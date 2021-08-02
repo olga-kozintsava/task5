ba375a4010e1b63d2034db15674abe827ebe960e
olga-kozintsava <elolo123654@gmail.com>
2021-08-02 19:14:26 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+
 Route::get('/', 'MainPageController@get');
 Route::post('/save', 'MainPageController@save');
